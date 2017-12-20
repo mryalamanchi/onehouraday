51c8d0696c7da2fe4228ddefb80681c147572b33
gawadaz <gawad.azzam@gmail.com>
2017-12-20 23:50:22 +0200

    my changes

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/assets', express.static('public'));
+app.use('/static', express.static('frontend'));
 app.use('/bower', express.static('bower_components'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/frontend/index.html');
 });
 
-  app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-  });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
