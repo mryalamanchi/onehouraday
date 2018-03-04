866f23008c7abcb920c2461100908476cb092aca
Alex Munoz <almunozdo@gmail.com>
2018-03-04 11:09:12 -0300

    fix eslint and webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,51 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './client/',
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader',
+          options: { cacheDirectory: true }
         }
       },
       {
         test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+        use: {
+          loader: 'html-loader',
+          options: { minimize: true }
+        }
       },
       {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-        },
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: 'url-loader',
+        options: { limit: 10000 }
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        exclude: [/\.(js|jsx)$/, /\.html$/, /\.json$/],
+        loader: 'file-loader',
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./client/template.html"
+      template: './client/template.html'
     })
   ]
-}
+};
