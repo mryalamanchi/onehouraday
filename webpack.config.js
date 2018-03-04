b8799aeec3fb06dba957f5c0ce11adc84556216d
Alex Munoz <almunozdo@gmail.com>
2018-03-04 11:26:56 -0300

    fix duplicate loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         enforce: 'pre',
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
+        use: 'eslint-loader'
       },
       {
         test: /\.(js|jsx)$/,
