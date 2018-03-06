76e65f918fa03094911c50c8127d9e02328c9b99
debjohnson33 <debjohnson_11@yahoo.com>
2018-03-06 14:45:48 -0600

    Added simple Search component/added it to router & routes

--- client/routes.js
+++ client/routes.js
@@ -8,6 +8,7 @@ import {
 
 import Home from './containers/Home';
 import About from './containers/About';
+import Search from './containers/Search';
 
 const RouterConfig = () => (
   <Router>
@@ -15,10 +16,12 @@ const RouterConfig = () => (
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
+        <li><Link to="/search">Search</Link></li>
       </ul>
 
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/search" component={Search} />
     </div>
   </Router>
 );
