88052098d5c39444693f35bd73967ff420113764
debjohnson33 <debjohnson_11@yahoo.com>
2018-03-06 18:59:14 -0600

    Added input box and component state to Search component/changed to class

--- client/containers/Search.js
+++ client/containers/Search.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Search = () => (
-	<h1>Search</h1>
-);
+class Search extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			search: ''
+		};
+	}
+
+	updateSearch(event) {
+		this.setState({search: event.target.value});
+	}
+
+	render() {
+		let filteredProjects = this.props.projects.filter(
+			(project) => {
+				return project.name.indexOf(this.state.search) !== -1;
+			}
+		);
+		return (
+			<div>
+				<h1>Search</h1>
+				<input type="text" 
+					value={this.state.search}
+					onChange={this.updateSearch.bind(this)}
+				/>
+				<ul>
+					{filteredProjects.map((project) => {
+						return <Project project={project} key={project.id} />
+					})}
+				</ul>
+			</div>
+		)
+	}
+}
 
 export default Search;
