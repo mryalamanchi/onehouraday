c554d494799c8e51977f26b7525d42dcb03b5184
Shilpi Verma <shilpiverma@Shilpis-MacBook-Pro.local>
2018-03-07 15:56:40 -0600

    adding email/password Authentication

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
-import mongoose from 'mongoose';
-
+const mongoose = require('mongoose');
+const validator = require ('validator');
+const bcrypt = require ('bcryptjs');
 
 const UserSchema = mongoose.Schema({
     email: {
@@ -8,12 +9,38 @@ const UserSchema = mongoose.Schema({
         lowercase:true,
         index: { unique: true },
         trim:true,
+        validator:{
+          validator:validator.isEmail,
+          message:'{VALUE} is not a valid email'
+        }
       },
       password: {
         type: String,
         required: true,
-        trim:true
       },
-
-})
-export default mongoose.model('User', UserSchema);
+});
+UserSchema.pre('save',function(next){
+  var user = this;
+  if(user.isModified('password')){
+    bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(user.password,salt,(err,hash)=>{
+        if (err) {
+          return next(err);
+        }
+        user.password=hash;
+        next();
+      });
+    });
+  }else{
+    return next();
+  }
+});
+UserSchema.methods.comparePassword = function(password, cb) {  
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+module.exports = mongoose.model('User', UserSchema);
