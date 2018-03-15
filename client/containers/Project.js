4411a250845f94e470eba465576a7162393f8c14
debjohnson33 <debjohnson_11@yahoo.com>
2018-03-15 13:19:24 -0500

    Added Project component/made input into form/added handleOnSubmit with console log

---
+++ client/containers/Project.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+	
+	render() {
+		
+		return (
+			<div>
+				<h3>Project info here... </h3>
+			</div>
+		)
+	}	
+}
+
+export default Project;
--- client/containers/Search.js
+++ client/containers/Search.js
@@ -9,28 +9,33 @@ class Search extends Component {
 		};
 	}
 
-	updateSearch(event) {
-		this.setState({search: event.target.value});
+	handleChange = event => {
+		this.setState({
+			search: event.target.value,
+		});
+	}
+
+	handleOnSubmit = event => {
+		event.preventDefault();
+		const { search } = this.state;
+		//send request to DB for specified project(s) that match search		
+		//set filteredProjects equal to result of DB request
+		console.log(search);
 	}
 
 	render() {
-		let filteredProjects = this.props.projects.filter(
-			(project) => {
-				return project.name.indexOf(this.state.search) !== -1;
-			}
-		);
+	
 		return (
 			<div>
 				<h1>Search</h1>
-				<input type="text" 
-					value={this.state.search}
-					onChange={this.updateSearch.bind(this)}
-				/>
-				<ul>
-					{filteredProjects.map((project) => {
-						return <Project project={project} key={project.id} />
-					})}
-				</ul>
+				<form onSubmit={this.handleOnSubmit}>
+					<input type="text" 
+						value={this.state.search}
+						onChange={this.handleChange.bind(this)}
+					/>
+					<button>Search</button>
+				</form>
+					<Project />	
 			</div>
 		)
 	}
