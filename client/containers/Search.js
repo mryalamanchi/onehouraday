39e4e140135283e9c9ff2df5e673da2346dcc848
debjohnson33 <debjohnson_11@yahoo.com>
2018-03-19 15:21:50 -0500

    Took out tabs, extra spaces, fixed syntax

--- client/containers/Search.js
+++ client/containers/Search.js
@@ -1,44 +1,42 @@
 import React, { Component } from 'react';
+import Project from './Project';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    };
+  }
 
-	constructor() {
-		super();
-		this.state = {
-			search: ''
-		};
-	}
+  handleChange(event) {
+    this.setState({
+      search: event.target.value
+    });
+  }
 
-	handleChange = event => {
-		this.setState({
-			search: event.target.value,
-		});
-	}
+  handleOnSubmit(event) {
+    event.preventDefault();
+    const { search } = this.state;
+    //send request to DB for specified project(s) that match search		
+    //set filteredProjects equal to result of DB request
+  }
 
-	handleOnSubmit = event => {
-		event.preventDefault();
-		const { search } = this.state;
-		//send request to DB for specified project(s) that match search		
-		//set filteredProjects equal to result of DB request
-		console.log(search);
-	}
-
-	render() {
-	
-		return (
-			<div>
-				<h1>Search</h1>
-				<form onSubmit={this.handleOnSubmit}>
-					<input type="text" 
-						value={this.state.search}
-						onChange={this.handleChange.bind(this)}
-					/>
-					<button>Search</button>
-				</form>
-					<Project />	
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h1>Search</h1>
+        <form onSubmit={this.handleOnSubmit}>
+          <input type="text"
+            value={this.state.search}
+            onChange={this.handleChange.bind(this)}
+          />
+          <button>Search</button>
+        </form>
+        <Project />
+      </div>
+    );
+  }
 }
 
 export default Search;
