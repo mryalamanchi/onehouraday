7c6e958381329d6d34228106687d9cb59cbfeed1
thecesp <filomena.andrea@hotmail.it>
2018-03-23 22:51:27 +0100

    Code refactoring

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,24 +1,30 @@
 /* eslint react/prop-types: 0 */
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
+import classNames from 'classnames';
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 import Avatar from 'material-ui/Avatar';
-import Paper from 'material-ui/Paper';
 import handShake from '../images/hand_shake.svg';
 import wallClock from '../images/wall_clock.svg';
 import group from '../images/group.svg';
 import homeBanner from '../images/gum_homebanner.jpg';
 
-const styles = theme => ({
-  root: {
-    marginTop: -8
-  },
+const styles = () => ({
   headerArea: {
     width: 1280,
+    height: 550,
     margin: '0 auto',
-    padding: '20px 0',
-    background: `url(${homeBanner}) no-repeat center`
+    padding: '0px 20px',
+    background: `url(${homeBanner}) no-repeat top center`
+  },
+  topHeader: {
+    margin: 0
+  },
+  titleBox: {
+    alignSelf: 'center',
+    textAlign: 'left'
   },
   title: {
     fontFamily: 'Barrio',
@@ -32,18 +38,33 @@ const styles = theme => ({
     fontSize: 20,
     lineHeight: 'normal',
     letterSpacing: 3.4,
-    textAlign: 'left',
+    textAlign: 'center',
     color: '#ffffff'
   },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    width: '80%', // temporary box waiting for search box
-    visibility: 'hidden' // temporary box waiting for search box
+  countryBox: {
+    alignSelf: 'flex-start',
+    textAlign: 'left',
+    visibility: 'hidden'
   },
-  row: {
+  publishBox: {
+    alignSelf: 'flex-start',
+    textAlign: 'right',
+    visibility: 'hidden'
+  },
+  publishButton: {
+    fontSize: '14px',
+    fontWeight: 900,
+    color: 'rgba(255, 255, 255, 0.87)',
+    backgroundColor: '#4a90e2'
+  },
+  bottomHeader: {
+    margin: 0,
+    alignItems: 'flex-start'
+  },
+  valueBox: {
     display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   valueImageBox: {
     width: '108px',
@@ -65,75 +86,92 @@ const styles = theme => ({
     letterSpacing: 3.4,
     textAlign: 'left',
     color: '#ffffff'
+  },
+  flex1: {
+    flex: 1
+  },
+  flex2: {
+    flex: 2
   }
 });
 
 const Header = (props) => {
   const { classes } = props;
+  const topHeader = classNames(`${classes.topHeader}`, `${classes.flex1}`);
+  const countryBox = classNames(`${classes.countryBox}`);
+  const titleBox = classNames(`${classes.titleBox}`);
+  const publishBox = classNames(`${classes.publishBox}`);
+  const searchBox = classNames(`${classes.flex2}`);
+  const bottomHeader = classNames(`${classes.bottomHeader}`, `${classes.flex2}`);
+  const valueBox = classNames(`${classes.valueBox}`);
+
   return (
     <Grid
       container
       direction="column"
       justify="center"
       alignItems="stretch"
-      spacing={40}
       className={classes.headerArea}
     >
-
-      <Grid xs={12} item className={classes.row}>
-        <Typography variant="title" className={classes.title} gutterBottom>
-          One hour a Day
-        </Typography>
+      <Grid container xs={12} className={topHeader}>
+        <Grid xs={4} item className={countryBox}>
+          <Button variant="raised" color="primary">
+            Primary
+          </Button>
+        </Grid>
+        <Grid xs={4} item className={titleBox}>
+          <Typography variant="title" className={classes.title}>
+            One hour a Day
+          </Typography>
+        </Grid>
+        <Grid xs={4} item className={publishBox}>
+          <Button variant="raised" color="primary" className={classes.publishButton}>
+            Publish your social project
+          </Button>
+        </Grid>
       </Grid>
 
-      <Grid xs={12} item className={classes.row}>
-        <Typography variant="subheading" className={classes.subtitle} gutterBottom>
+      <Grid xs={12} item className={searchBox}>
+        <Typography variant="subheading" className={classes.subtitle}>
           Spend 1 hour and help a social cause
         </Typography>
       </Grid>
 
-      <Grid xs={12} item className={classes.row}>
-        <Paper className={classes.paper}>search box</Paper>
-      </Grid>
+      <Grid container xs={12} className={bottomHeader} justify="center">
 
-
-      <Grid item xs={12}>
-        <Grid container justify="center">
-
-          <Grid xs={4} item className={classes.row}>
-            <Avatar className={classes.valueImageBox}>
-              <img src={handShake} className={classes.valueImage} alt="handShake" />
-            </Avatar>
-            <div className={classes.valueTextBox}>
-              <Typography className={classes.valueText}>
-                Be a hero. Help an organisation and improve your skills
-              </Typography>
-            </div>
-          </Grid>
-
-          <Grid xs={4} item className={classes.row}>
-            <Avatar className={classes.valueImageBox}>
-              <img src={wallClock} className={classes.valueImage} alt="wallClock" />
-            </Avatar>
-            <div className={classes.valueTextBox}>
-              <Typography className={classes.valueText}>
-                All you need to dedicate is an hour
-              </Typography>
-            </div>
-          </Grid>
-
-          <Grid xs={4} item className={classes.row}>
-            <Avatar className={classes.valueImageBox}>
-              <img src={group} className={classes.valueImage} alt="group" />
-            </Avatar>
-            <div className={classes.valueTextBox}>
-              <Typography className={classes.valueText}>
-                A little collaboration from each of us can generate a huge impact
-              </Typography>
-            </div>
-          </Grid>
-
+        <Grid xs={4} item className={valueBox}>
+          <Avatar className={classes.valueImageBox}>
+            <img src={handShake} className={classes.valueImage} alt="handShake" />
+          </Avatar>
+          <div className={classes.valueTextBox}>
+            <Typography className={classes.valueText}>
+              Be a hero. Help an organisation and improve your skills
+            </Typography>
+          </div>
         </Grid>
+
+        <Grid xs={4} item className={valueBox}>
+          <Avatar className={classes.valueImageBox}>
+            <img src={wallClock} className={classes.valueImage} alt="wallClock" />
+          </Avatar>
+          <div className={classes.valueTextBox}>
+            <Typography className={classes.valueText}>
+              All you need to dedicate is an hour
+            </Typography>
+          </div>
+        </Grid>
+
+        <Grid xs={4} item className={valueBox}>
+          <Avatar className={classes.valueImageBox}>
+            <img src={group} className={classes.valueImage} alt="group" />
+          </Avatar>
+          <div className={classes.valueTextBox}>
+            <Typography className={classes.valueText}>
+              A little collaboration from each of us can generate a huge impact
+            </Typography>
+          </div>
+        </Grid>
+
       </Grid>
 
     </Grid>
