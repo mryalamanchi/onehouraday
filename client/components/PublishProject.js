86fecc5d3d275577b5a928ca997b8df2a750d88e
thecesp <filomena.andrea@hotmail.it>
2018-04-01 18:35:04 +0200

    publish project form completed before merge

--- client/components/PublishProject.js
+++ client/components/PublishProject.js
@@ -1,15 +1,14 @@
 /* eslint react/prop-types: 0 */
 import React from 'react';
+import classNames from 'classnames';
+import update from 'immutability-helper';
 import { withStyles } from 'material-ui/styles';
 import {
   FormLabel,
-  // FormControl,
   FormGroup,
   FormControlLabel,
   FormHelperText
 } from 'material-ui/Form';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
 import TextField from 'material-ui/TextField';
 import Checkbox from 'material-ui/Checkbox';
 import IconButton from 'material-ui/IconButton';
@@ -22,9 +21,9 @@ import HeaderBar from './HeaderBar';
 const styles = {
   root: {
     flexGrow: 1,
-    marginLeft: 60,
-    marginRight: 50,
-    marginBottom: 20
+    maxWidth: 1280,
+    margin: '20px auto',
+    backgroundColor: '#f8f9fc;'
   },
   flex: {
     display: 'flex'
@@ -39,6 +38,10 @@ const styles = {
     width: '100%',
     height: '100%'
   },
+  form: {
+    padding: 20,
+    backgroundColor: '#ffffff;'
+  },
   formRow: {
     marginTop: 30,
     marginBottom: 40
@@ -50,6 +53,9 @@ const styles = {
   formItem2Col: {
     paddingRight: 30,
     width: '50%'
+  },
+  counter: {
+    textAlign: 'right'
   },
   projectPhoto: {
     width: 160,
@@ -72,7 +78,13 @@ const styles = {
   boxIcon: {
     alignItems: 'center',
     padding: 10,
-    marginRight: 30
+    marginRight: 30,
+    width: 120
+  },
+  boxIconSelected: {
+    fontWeight: 'bold',
+    border: 'solid 1px #979797',
+    backgroundColor: '#D6D6D6'
   },
   icon: {
     width: 60,
@@ -100,16 +112,68 @@ const styles = {
 class PublishProject extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: ''
-    };
     // fetch from service?
     this.props.categories = ['EDUCATION', 'HUNGRY', 'POVERTY', 'HEALTH',
       'ENVIRONMENT', 'PEACE', 'JUSTICE', 'ANIMALS',
       'BUSINESS', 'HUMAN RIGHTS', 'OTHERS'];
 
+    this.state = {
+      categories: this.props.categories.map(category => ({ [`'${category}'`]: false })),
+      helpFromWorld: true,
+      helpFromLocation: false,
+      projectInfo: '',
+      tasks: [{ taskName: '', volunteerSkill: '' }],
+      showOwnerEmail: false
+    };
+
+    this.switchHelpLocation = this.switchHelpLocation.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.updateTask = this.updateTask.bind(this);
+    this.updateCategories = this.updateCategories.bind(this);
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  addTask() {
+    this.setState(prevState => ({
+      tasks: [...prevState.tasks, { taskName: '', volunteerSkill: '' }]
+    }));
+  }
+
+  updateTask(taskParam, index) {
+    return (event) => {
+      const val = event.target.value;
+      this.setState(prevState => ({
+        tasks: update(prevState.tasks, { [index]: { [taskParam]: { $set: val } } })
+      }));
+    };
+  }
+
+  updateCategories(elem, index) {
+    return (event) => {
+      const val = event.target.checked;
+      this.setState(prevState => ({
+        categories: update(prevState.categories, { [index]: { [`'${elem}'`]: { $set: val } } })
+      }));
+    };
+  }
+
+  switchHelpLocation(from, to) {
+    return (event) => {
+      this.setState({
+        [from]: event.target.checked,
+        [to]: !event.target.checked
+      });
+    };
+  }
+
+  handleCheckboxChange(name) {
+    return (event) => {
+      this.setState({
+        [name]: event.target.checked
+      });
+    };
   }
 
   handleChange(name) {
@@ -121,7 +185,6 @@ class PublishProject extends React.Component {
   }
 
   handleSubmit(event) {
-    console.log('A form was submitted: '); //  + this.state.value);
     console.log(this.state);
     event.preventDefault();
   }
@@ -138,7 +201,7 @@ class PublishProject extends React.Component {
       <div className={classes.root}>
         <HeaderBar param={headerParam} />
 
-        <form autoComplete="off">
+        <form autoComplete="off" className={classes.form}>
 
           <div className={classes.formRow}>
             <FormGroup className={classes.boxProjectPhoto}>
@@ -175,72 +238,83 @@ class PublishProject extends React.Component {
           </div>
 
           <FormLabel component="legend">What categories does your project belong to?</FormLabel>
-          <div >
-            {this.props.categories.map(category =>
-              (<FormControlLabel
-                className={classes.formItem3Col}
-                control={
-                  <Checkbox
-                    checked={this.state[category]}
-                    onChange={this.handleChange(category)}
-                    value={category}
-                  />
-                }
-                label={category}
-              />))}
-          </div>
+          {this.props.categories.map((category, index) =>
+            (<FormControlLabel
+              className={classes.formItem3Col}
+              control={
+                <Checkbox
+                  checked={this.state.categories[category]}
+                  onChange={this.updateCategories(category, index)}
+                  value={this.state.categories[category]}
+                />
+              }
+              label={category}
+            />))}
 
           <FormLabel component="legend" className={classes.formRow}>What&apos;s your project about?</FormLabel>
-
-          <FormLabel component="legend">Describe whats your project about</FormLabel>
           <div className={classes.formRow}>
             <FormGroup>
               <TextField
                 id="projectInfo"
                 value={this.state.projectInfo}
                 onChange={this.handleChange('projectInfo')}
+                label="Describe whats your project about"
                 fullWidth
                 margin="normal"
               />
             </FormGroup>
+            <FormHelperText className={classes.counter}>
+              {this.state.projectInfo.length}/250
+            </FormHelperText>
           </div>
 
 
           <FormLabel component="legend">What tasks needs to be done</FormLabel>
-          <div className={classNames(`${classes.formRow}`, `${classes.flex}`)}>
-            <TextField
-              className={classes.formItem2Col}
-              id="taskName"
-              label="Task name"
-              value={this.state.taskName}
-              onChange={this.handleChange('taskName')}
-            />
-            <TextField
-              className={classes.formItem2Col}
-              id="volunteerRole"
-              label="Volunteer skill"
-              value={this.state.volunteerSkill}
-              onChange={this.handleChange('volunteerSkill')}
-            />
-          </div>
+          {this.state.tasks.map((task, index) =>
+            (
+              <div className={classNames(`${classes.formRow}`, `${classes.flex}`)}>
+                <TextField
+                  className={classes.formItem2Col}
+                  id="taskName"
+                  label="Task name"
+                  value={task.taskName}
+                  onChange={this.updateTask('taskName', index)}
+                />
+                <TextField
+                  className={classes.formItem2Col}
+                  id="volunteerSkill"
+                  label="Volunteer Skill"
+                  value={task.volunteerSkill}
+                  onChange={this.updateTask('volunteerSkill', index)}
+                />
+              </div>
+            ))}
 
           <div className={classes.formRow}>
-            <Link to="/" className={classes.link}>Add</Link>
+            <Button onClick={this.addTask} className={classes.link}>Add</Button>
           </div>
 
 
           <FormLabel component="legend" className={classes.formRow}>From where this project needs help?</FormLabel>
           <div className={classNames(`${classes.formRow}`, `${classes.flex}`)}>
-            <FormGroup className={classes.boxIcon}>
-              <IconButton>
-                <Public className={classes.icon} />
-              </IconButton>
+            <FormGroup className={classNames(`${classes.boxIcon}`, `${this.state.helpFromWorld ? classes.boxIconSelected : ''}`)}>
+              <Checkbox
+                id="helpFromWorld"
+                icon={<Public className={classes.icon} />}
+                checkedIcon={<Public className={classes.icon} />}
+                checked={this.state.helpFromWorld}
+                onChange={this.switchHelpLocation('helpFromWorld', 'helpFromLocation')}
+              />
               <FormHelperText className={classes.textIcon}>Entire World</FormHelperText>
             </FormGroup>
-            <FormGroup className={classes.boxIcon}>
-              <IconButton>
-                <Place className={classes.icon} />
-              </IconButton>
+            <FormGroup className={classNames(`${classes.boxIcon}`, `${this.state.helpFromLocation ? classes.boxIconSelected : ''}`)}>
+              <Checkbox
+                id="helpFromLocation"
+                icon={<Place className={classes.icon} />}
+                checkedIcon={<Place className={classes.icon} />}
+                checked={this.state.helpFromLocation}
+                onChange={this.switchHelpLocation('helpFromLocation', 'helpFromWorld')}
+              />
               <FormHelperText className={classes.textIcon}>Specific Location</FormHelperText>
             </FormGroup>
           </div>
@@ -269,8 +343,8 @@ class PublishProject extends React.Component {
                 control={
                   <Checkbox
                     checked={this.state.showOwnerEmail}
-                    onChange={this.handleChange('showOwnerEmail')}
-                    value="jason"
+                    onChange={this.handleCheckboxChange('showOwnerEmail')}
+                    value={this.state.showOwnerEmail}
                   />
                   }
                 label="Show my email"
@@ -278,9 +352,8 @@ class PublishProject extends React.Component {
             </FormGroup>
           </div>
 
-
           <div className={classes.formRow}>
-            <Link to="/" className={classes.link}>Publish</Link>
+            <Button onClick={this.handleSubmit} className={classes.link}>Publish</Button>
           </div>
 
         </form>
