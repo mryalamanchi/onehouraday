5d67fad855b582fef92ee729fb217b8a52d49b29
Nik Bornemeier <NikolausRB@gmail.com>
2018-04-05 14:03:35 -0600

    add CategoryNavItem add propTypes.

--- client/scenes/Home/components/CategoryNavigation/components/NavColumn/index.js
+++ client/scenes/Home/components/CategoryNavigation/components/NavColumn/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { object, array } from 'prop-types';
 import Grid from 'material-ui/Grid';
+import CategoryNavItem from './components/CategoryNavItem';
 
 const NavColumn = (props) => {
-  const { categories } = props;
-  const columnCategories = categories.map(category=> {
-    <CategorynavItem category={category}/>
-  });
+  const { categories, classes } = props;
+  const columnCategories = categories.map(category =>
+    <CategoryNavItem projectItemClass={classes.projectItem} category={category} />);
 
   return (
     <Grid container item spacing={0} direction="column" xs="4">
-      { columnCategoires }
+      { columnCategories }
     </Grid>
-  )
+  );
 };
 
-export const NavColumn;
+NavColumn.propTypes = {
+  /* eslint react/forbid-prop-types: 0 */
+  classes: object.isRequired,
+  categories: array.isRequired
+};
+
+export default NavColumn;
